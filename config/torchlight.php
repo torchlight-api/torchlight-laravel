d1e1bd06f751a02dc70acffe5108ddad1955f99c
Aaron Francis <aarondfrancis@gmail.com>
2021-07-28 08:27:17 -0500

    Don't ship any defaults, so we don't override people's current configs

--- config/torchlight.php
+++ config/torchlight.php
@@ -23,17 +23,17 @@ return [
     // https://torchlight.dev/docs/options
     'options' => [
         // Turn line numbers on or off globally.
-        'lineNumbers' => true,
+        // 'lineNumbers' => false,
 
         // Control the `style` attribute applied to line numbers.
         // 'lineNumbersStyle' => '',
 
         // Turn on +/- diff indicators.
-        'diffIndicators' => true,
+        // 'diffIndicators' => true,
 
         // If there are any diff indicators for a line, put them
         // in place of the line number to save horizontal space.
-        'diffIndicatorsInPlaceOfLineNumbers' => true,
+        // 'diffIndicatorsInPlaceOfLineNumbers' => true,
 
         // When lines are collapsed, this is the text that will
         // be shown to indicate that they can be expanded.
